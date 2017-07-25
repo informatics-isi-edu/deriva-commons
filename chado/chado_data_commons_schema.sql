342154441d2819e93b828af2a5b691e739197703
Laura Pearlman <laura@isi.edu>
2017-07-24 18:02:46 -0700

    Separated schema creation and chado-to-data-commons transformation into separate files.

---
+++ chado/chado_data_commons_schema.sql
@@ -0,0 +1,71 @@
+\set dc data_commons
+
+begin;
+create schema :dc;
+
+create table :dc.db (
+  name text primary key,
+  description text,
+  urlprefix text,
+  url text
+  );
+
+create table :dc.cv (
+  name text primary key,
+  definition text
+);
+
+create table :dc.dbxref (
+  name text primary key,
+  db text not null references :dc.db(name) deferrable,
+  accession text not null,
+  version text not null default '',
+  description text,
+  unique(db, accession, version)
+);
+
+create table :dc.cvterm (
+  dbxref text primary key references :dc.dbxref(name) deferrable,
+  dbxref_unversioned text not null,
+  cv text not null,
+  name text not null,
+  definition text,
+  is_obsolete boolean not null,
+  is_relationshiptype boolean not null,
+  synonyms text[],
+  alternate_dbxrefs text[],
+  unique(cv, name, is_obsolete),
+  check(dbxref like dbxref_unversioned || ':%')
+);
+
+create index on :dc.cvterm(dbxref_unversioned);
+-- Ugh. These next indexes are used in searches of the form '{term}' <@ synonyms but not 'term' = any(synonyms)
+create index on :dc.cvterm using gin(synonyms);
+create index on :dc.cvterm using gin(alternate_dbxrefs);
+
+create table :dc.cvtermsynonym (
+  cvtermsynonym_id bigserial primary key,
+  dbxref text not null references :dc.cvterm(dbxref) deferrable,
+  synonym text not null,
+  synonym_type text,
+  unique (dbxref, synonym)
+);
+
+create table :dc.cvterm_dbxref (
+  cvterm_dbxref_id bigserial primary key,
+  primary_dbxref text not null references :dc.cvterm(dbxref) deferrable,
+  alternate_dbxref text not null references :dc.dbxref(name) deferrable,
+  is_for_definition boolean,
+  unique(primary_dbxref, alternate_dbxref)
+);
+
+create table :dc.cvtermpath (
+  cvtermpath_id bigserial primary key,
+  type_dbxref text not null references :dc.cvterm(dbxref) deferrable,
+  subject_dbxref text not null references :dc.cvterm(dbxref) deferrable,
+  object_dbxref text not null references :dc.cvterm(dbxref) deferrable,
+  pathdistance integer not null,
+  unique(type_dbxref, subject_dbxref, object_dbxref)
+);  
+
+commit;
--- chado/chado_to_data_commons.sql
+++ chado/chado_to_data_commons.sql
@@ -2,72 +2,12 @@
 \set chado public
 
 begin;
-create schema :dc;
+
 create or replace function :dc.chado_dbxref_id_to_dbxref(dbxref_id bigint) returns text as $$
   select d.name || ':' || x.accession || ':' || x.version
     from public.dbxref x join public.db d on d.db_id = x.db_id
     where x.dbxref_id = $1;
 $$ language sql;
-
-create table :dc.db (
-  name text primary key,
-  description text,
-  urlprefix text,
-  url text
-  );
-
-create table :dc.cv (
-  name text primary key,
-  definition text
-);
-
-create table :dc.dbxref (
-  name text primary key,
-  db text not null references :dc.db(name) deferrable,
-  accession text not null,
-  version text not null default '',
-  description text,
-  unique(db, accession, version)
-);
-
-create table :dc.cvterm (
-  dbxref text primary key references :dc.dbxref(name) deferrable,
-  dbxref_unversioned text not null,
-  cv text not null,
-  name text not null,
-  definition text,
-  is_obsolete boolean not null,
-  is_relationshiptype boolean not null,
-  synonyms text[],
-  alternate_dbxrefs text[],
-  unique(cv, name, is_obsolete),
-  check(dbxref like dbxref_unversioned || ':%')
-);
-
-create table :dc.cvtermsynonym (
-  cvtermsynonym_id bigserial primary key,
-  dbxref text not null references :dc.cvterm(dbxref) deferrable,
-  synonym text not null,
-  synonym_type text,
-  unique (dbxref, synonym)
-);
-
-create table :dc.cvterm_dbxref (
-  cvterm_dbxref_id bigserial primary key,
-  primary_dbxref text not null references :dc.cvterm(dbxref) deferrable,
-  alternate_dbxref text not null references :dc.dbxref(name) deferrable,
-  is_for_definition boolean,
-  unique(primary_dbxref, alternate_dbxref)
-);
-
-create table :dc.cvtermpath (
-  cvtermpath_id bigserial primary key,
-  type_dbxref text not null references :dc.cvterm(dbxref) deferrable,
-  subject_dbxref text not null references :dc.cvterm(dbxref) deferrable,
-  object_dbxref text not null references :dc.cvterm(dbxref) deferrable,
-  pathdistance integer not null,
-  unique(type_dbxref, subject_dbxref, object_dbxref)
-);  
 
 insert into :dc.db(name, description, urlprefix, url)
   select name, description, urlprefix, url from :chado.db;
