ab15633ba5a9d94d48753dc6c11b24cff54f7cb4
Laura Pearlman <laura@Laura-Pearlmans-MacBook-Pro.local>
2020-04-30 16:19:02 -0700

    Add common parameters (parameters that no one is likely to want to change for any catalog here) to chaise-config.js

--- demo-db/chaise-config.js
+++ demo-db/chaise-config.js
@@ -1,6 +1,9 @@
 // Configure deployment-specific data in the ermrest catalog, not here.
 
 var chaiseConfig = {
+    deleteRecord: true,
+    editRecord: true,
+    showFaceting: true
 };
 
 if (typeof module === 'object' && module.exports && typeof require === 'function') {
