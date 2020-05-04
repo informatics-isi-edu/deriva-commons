5112eae6702f12ac8fa4668a5619ccd6fc863b2c
Laura Pearlman <laura@Laura-Pearlmans-MacBook-Pro.local>
2020-05-04 14:41:42 -0700

    Uncomment a line I commented for testing.

--- demo-db/hatrac-init.sh
+++ demo-db/hatrac-init.sh
@@ -1,7 +1,7 @@
 host=tutorial.derivacloud.org
 isrd_systems="https://auth.globus.org/3938e0d0-ed35-11e5-8641-22000ab4b42b"
 
-#deriva-hatrac-cli --host $host mkdir --parents /hatrac/resources
+deriva-hatrac-cli --host $host mkdir --parents /hatrac/resources
 deriva-hatrac-cli --host $host setacl /hatrac/resources owner $isrd_systems
 deriva-hatrac-cli --host $host setacl /hatrac/resources subtree-owner $isrd_systems
 deriva-hatrac-cli --host $host setacl /hatrac/resources subtree-read "*"
